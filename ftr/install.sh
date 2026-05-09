2bd7d59821fa5b30289137b345aeb4fac2e7e292
SQU1DMAN6 <quanminhthai@outlook.com>
2026-05-09 21:22:37 +1000

    Starting on FtR 3.1

--- ftr/install.sh
+++ ftr/install.sh
@@ -18,7 +18,7 @@ echo "Creating share directory at /usr/local/share/ftr..."
 sudo mkdir -p "/usr/local/share/ftr"
 
 echo "Building FtR..."
-go build -o ftr . -buildvcs=falses
+go build -o ftr . -buildvcs=false
 echo "Making binary executable..."
 chmod 755 ./ftr
 
